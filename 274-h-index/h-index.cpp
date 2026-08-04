ebc6881c6f478771317ebdd77fdeba233f85a393
Rupender Singh <163423981+rupender2005@users.noreply.github.com>
2026-08-04 21:22:35 +0530

    Time: 3 ms (13.74%) | Memory: 12.2 MB (85.62%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        reverse(citations.begin(),citations.end());
+        int s=citations.size();
+        int ans=0;
+        for(int i=0;i<s;i++){
+            if(citations[i]>=i+1){
+                ans=i+1;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
