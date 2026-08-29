613fd06b8712dc8b3c1108ec51002f34b0cec127
Rupender Singh <163423981+rupender2005@users.noreply.github.com>
2026-08-29 22:48:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.16%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int s=nums.size();
+        int a=0;
+        int b=0;
+        int c=0;
+        for(int i=0;i<s;i++){
+            if(nums[i]==0){
+                a++;
+            }
+            if(nums[i]==1){
+                b++;
+            }
+            if(nums[i]==2){
+                c++;
+            }
+        }
+        for(int i=0;i<a;i++){
+            nums[i]=0;
+        }
+        for(int i=a;i<a+b;i++){
+            nums[i]=1;
+        }
+        for(int i=a+b;i<a+b+c;i++){
+            nums[i]=2;
+        }
+    }
+};
